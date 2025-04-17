94702360a6872e6dade048a55dc962b365fd1d74
Farran Ahmed <82695487+Farran-Ahmed@users.noreply.github.com>
2025-04-17 17:04:05 -0500

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,9 +1,6 @@
-/* Farran Ahmed
- * 2170-001
- * Filename: board.h
-* Description: header file using struc and class 
- * A.I. Disclaimer: All work for this assignment was completed by myself and entirely without the use of artificial intelligence tools such as ChatGPT, MS Copilot, other LLMs, etc.
-*/
+
+ //Filename: board.h
+
 
 #ifndef BOARD_H
 #define BOARD_H
